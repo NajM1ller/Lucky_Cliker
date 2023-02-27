33cc1e59b40e34679b3493fc713879b153b1dd59
NajM1ller <126503314+NajM1ller@users.noreply.github.com>
2023-02-28 02:25:48 +0700

    Add files via upload

--- main.js
+++ main.js
@@ -1,7 +1,14 @@
 var A=document.getElementById("клик");
 var B=document.getElementById("клики");
-var C=0;
+let C=995;
+const D=1000;
+let F=50;
+var sound=document.getElementById("Sound1");
 B.addEventListener("click",function(){
-  C++;
-  A.textContent="кликов сделано "+C;
+  C++; 
+  if (C == 1000) { 
+    sound.play();
+    console.log('музыка должна играть');
+}
+  A.textContent="кликов сделано "+C; console.log(C);
 });
